82f229d14767fffb02eb05eb914eea8db694f63f
Oleksandrkhmilepam <oleksandr_khmil@epam.com>
2026-02-19 06:37:48 +0300

    Delete task12-restaurant directory

--- task12-restaurant/pyapp/tests/test_api_handler/test_success.py
+++
@@ -1,8 +0,0 @@
-from pyapp.tests.test_api_handler import ApiHandlerLambdaTestCase
-
-
-class TestSuccess(ApiHandlerLambdaTestCase):
-
-    def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), 200)
-
